d30fc20a1b5d5e49f32f1f721f031a13c81c3a76
WoonHak Kong <woonhak.kong@georgebrown.ca>
2021-04-04 17:47:38 +0900

    UI is implemented

--- GameSimulator.cpp
+++ GameSimulator.cpp
@@ -2,6 +2,11 @@
 #include "Store.h"
 #include "Player.h"
 #include <iostream>
+#include <iomanip>
+
+#include "Gun.h"
+#include "Sword.h"
+#include "Wand.h"
 
 using namespace std;
 
@@ -18,11 +23,38 @@ GameSimulator::~GameSimulator()
 void GameSimulator::Start()
 {
 	//Store* store = new Store();
-	//Player player;
+	Player player;
 	//store->EnterStore(player);
 	//delete store;
 
-	ShowGameUI();
+	player.AddWeapon(new Sword("Dragon_Slayer", "Kill the Dragon!!", 1000));
+	player.AddWeapon(new Sword("Excalibur", "Excalibur!!!!!", 2000));
+	player.AddWeapon(new Sword("Storm_Blinger", "Lighting!!", 1500));
+	player.AddWeapon(new Sword("Asura", "Asura Pa Chun Mu!!", 5000));
+	player.AddWeapon(new Gun("Flamethrower", "phu~~~~~~~~~", 1000));
+	player.AddWeapon(new Gun("Shot_Gun", "Shot!!", 1500));
+	player.AddWeapon(new Gun("Pistol", "Bang!!", 500));
+	player.AddWeapon(new Gun("Sniper", "slient kill!!", 2000));
+	player.AddWeapon(new Wand("Ice_Wand", "Fire Ball!!", 2000));
+	player.AddWeapon(new Wand("Fire_Wand", "Ice Ball!!", 2000));
+
+	ShowGameUI(player);
+
+
+	ChangeCC(COLOR::WHITE);
+	int select = 0;
+
+	do
+	{
+		ChangeCursorPlace(Q_PLACE.X, Q_PLACE.Y + 10);
+		cout << "Choose your weapon on right side by type number";
+		ChangeCursorPlace(Q_PLACE.X, Q_PLACE.Y + 11);
+		cout << "> ";
+		cin >> select;
+
+	} while (true);
+
+
 
 }
 
@@ -39,16 +71,80 @@ void GameSimulator::ChangeCursorPlace(int x , int y)
 	SetConsoleCursorPosition(m_windowsHandle, tmp);
 }
 
-void GameSimulator::ShowGameUI()
+void GameSimulator::ShowGameUI(Player player)
 {
+	ChangeCC(COLOR::YELLOW);
 	PrintSquare(Q_PLACE);
+	PrintSquare(W_PLACE);
+	PrintSquare(E_PLACE);
+	PrintSquare(R_PLACE);
+	PrintSquare(F_PLACE);
+	ChangeCC(COLOR::CYAN);
+	ChangeCursorPlace(Q_PLACE.X + 4, Q_PLACE.Y - 1);
+	cout << "    [Q]";
+	ChangeCursorPlace(W_PLACE.X + 4, W_PLACE.Y - 1);
+	cout << "    [W]";
+	ChangeCursorPlace(E_PLACE.X + 4, E_PLACE.Y - 1);
+	cout << "    [E]";
+	ChangeCursorPlace(R_PLACE.X + 4, R_PLACE.Y - 1);
+	cout << "    [R]";
+	ChangeCursorPlace(F_PLACE.X + 4, F_PLACE.Y - 1);
+	cout << "    [F]";
+	ChangeCC(COLOR::WHITE);
 
+
+
+	ShowPlayerItem(player);
+
+
+	ChangeCursorPlace(1, 30);
 }
 
 void GameSimulator::PrintSquare(COORD position)
 {
 	ChangeCursorPlace(position.X, position.Y);
-	cout << "====================\n";
-	ChangeCursorPlace(position.X, position.Y+2);
-	cout << "====================\n";
+	cout << "O------------------O";
+	ChangeCursorPlace(position.X, position.Y + 1);
+	cout << "|                  |";
+	ChangeCursorPlace(position.X, position.Y + 2);
+	cout << "O------------------O";
+}
+
+void GameSimulator::ShowPlayerItem(Player player)
+{
+
+
+	ChangeCC(COLOR::CYAN);
+	ChangeCursorPlace(ITEM_SHWING_PLACE.X, ITEM_SHWING_PLACE.Y - 1);
+	cout << "     [YOUR WEAPONS]";
+
+	ChangeCC(COLOR::YELLOW);
+	ChangeCursorPlace(ITEM_SHWING_PLACE.X, ITEM_SHWING_PLACE.Y);
+	cout << "O----------------------O";
+	int i = 1;
+	for (; i <= player.getWeaponList().size() * 2; i++)
+	{
+		ChangeCursorPlace(ITEM_SHWING_PLACE.X, ITEM_SHWING_PLACE.Y + i );
+		cout << "|                      |";
+	}
+	ChangeCursorPlace(ITEM_SHWING_PLACE.X, ITEM_SHWING_PLACE.Y + i);
+	cout << "O----------------------O";
+
+
+	for (auto iter = player.getWeaponList().begin(); iter != player.getWeaponList().end(); iter++)
+	{
+		ChangeCursorPlace(ITEM_SHWING_PLACE.X + 2, (ITEM_SHWING_PLACE.Y - 1) + (*iter).first * 2);
+		ChangeCC(COLOR::RED);
+		cout << setw(2) << left <<(*iter).first << ". ";
+		ChangeCC(COLOR::GREEN);
+		cout << (*iter).second->getName();
+	}
+
+	//for (i = 0; i < player.getWeaponList().size(); i++)
+	//{
+	//	ChangeCursorPlace(ITEM_SHWING_PLACE.X, ITEM_SHWING_PLACE.Y + i * 2);
+	//	cout << player.getWeaponList()[i+1]->getName();
+	//}
+
+
 }
--- GameSimulator.h
+++ GameSimulator.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Windows.h>
+#include "Player.h"
 
 
 
@@ -30,16 +31,25 @@ private:
 
 	void ChangeCC(COLOR color);
 	void ChangeCursorPlace(int x, int y);
-	void ShowGameUI();
+	void ShowGameUI(Player player);
 	void PrintSquare(COORD position);
+	void ShowPlayerItem(Player player);
 
 private:
 
 	const COORD Q_PLACE = { 10, 2 };
-	const COORD W_PLACE = { 20, 2 };
-	const COORD E_PLACE = { 30, 2 };
-	const COORD R_PLACE = { 40, 2 };
-	const COORD F_PLACE = { 50, 2 };
+	const COORD W_PLACE = { 32, 2 };
+	const COORD E_PLACE = { 54, 2 };
+	const COORD R_PLACE = { 76, 2 };
+	const COORD F_PLACE = { 76, 6 };
+	const COORD ITEM_SHWING_PLACE = { 120, 6 };
+
+
+	const COORD Q_NAME_PLACE = { Q_PLACE.X + 2, Q_PLACE.Y + 1 };
+	const COORD W_NAME_PLACE = { W_PLACE.X + 2, W_PLACE.Y + 1 };
+	const COORD E_NAME_PLACE = { E_PLACE.X + 2, E_PLACE.Y + 1 };
+	const COORD R_NAME_PLACE = { R_PLACE.X + 2, R_PLACE.Y + 1 };
+	const COORD F_NAME_PLACE = { F_PLACE.X + 2, F_PLACE.Y + 1 };
 
 
 
